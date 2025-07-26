a4d93c55753aad6499942d15e39b16c665603807
Jenkins, Kenneth Alexander <kjenkins60@gatech.edu>
2025-07-26 19:47:59 -0400

    actual final round of pushes
    Signed-off-by: Jenkins, Kenneth Alexander <kjenkins60@gatech.edu>

--- index.js
+++ index.js
@@ -7,16 +7,18 @@
  * The JavaScript code in this file is free software: you can
  * redistribute it and/or modify it under the terms of the GNU
  * General Public License (GNU GPL) as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option)
- * any later version.  The code is distributed WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+ * Foundation, version 3 of the License.  The code is distributed 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU GPL 
+ * for more details.
  *
  * As additional permission under GNU GPL version 3 section 7, you
  * may distribute non-source (e.g., minimized or compacted) forms of
  * that code without the copy of the GNU GPL normally required by
  * section 4, provided you include this license notice and a URL
  * through which recipients can access the Corresponding Source.
+ *
+ * Source: https://github.com/rockenman1234/rockenman1234.github.io/blob/main/index.js
  *
  * @licend  The above is the entire license notice
  * for the JavaScript code in this file.

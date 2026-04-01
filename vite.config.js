050d8026309590f4fab6e9a5e5d6b76dde3cdc26
Jenkins, Kenneth Alexander <kjenkins60@gatech.edu>
2026-03-31 20:56:31 -0400

    update deps, add compression for p2p sites
    Signed-off-by: Jenkins, Kenneth Alexander <kjenkins60@gatech.edu>

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,40 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { compression } from 'vite-plugin-compression2';
 
 export default defineConfig({
   base: '/',
   publicDir: 'public',
+  plugins: [
+    ViteImageOptimizer({
+      png: { quality: 60 },
+      jpeg: { quality: 90 },
+      jpg: { quality: 90 },
+      webp: { quality: 90 },
+      gif: { optimizationLevel: 3, interlaced: true },      svg: {
+        plugins: [
+          {
+            name: 'preset-default',
+            params: {
+              overrides: {
+                cleanupIds: false,
+                minifyStyles: false,
+              },
+            },
+          },
+        ],
+      },      exclude: /\.avif$/i,
+    }),
+    compression({
+      algorithm: 'gzip',
+      exclude: [/\.(br)$ /, /\.(gz)$/],
+    }),
+    compression({
+      algorithm: 'brotliCompress',
+      exclude: [/\.(br)$ /, /\.(gz)$/],
+    }),
+  ],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
